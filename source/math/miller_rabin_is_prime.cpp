b3a6e81c6714b78752eede85fce5530bb701642d
Timofey Remeslennikov <superxxxboom@gmail.com>
2018-12-01 02:36:33 +0300

    Add fast factorization, mincut description, troubleshoots note

---
+++ source/math/miller_rabin_is_prime.cpp
@@ -0,0 +1,16 @@
+bool prime(ull p) {
+	if (p == 2) return true;
+	if (p == 1 || p % 2 == 0) return false;
+	ull s = p - 1;
+	while (s % 2 == 0) s /= 2;
+	for (int i = 0 i < 15; i++) {
+		ull a = rand() % (p - 1) + 1, tmp = s;
+		ull mod = mod_pow(a, tmp, p);
+		while (tmp != p - 1 && mod != 1 && mod != p - 1) {
+			mod = mod_mul(mod, mod, p);
+			tmp *= 2;
+		}
+		if (mod != p - 1 && tmp % 2 == 0) return false;
+	}
+	return true;
+}
